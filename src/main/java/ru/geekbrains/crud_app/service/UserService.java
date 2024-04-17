92e3bd26bcc8c6d56b051ab019a6b2feacf51df3
вадим <funpalka1@gmail.com>
2024-04-18 00:48:45 +0400

    Initial commit

---
+++ src/main/java/ru/geekbrains/crud_app/service/UserService.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.crud_app.service;
+
+import org.springframework.stereotype.Service;
+import ru.geekbrains.crud_app.model.User;
+import ru.geekbrains.crud_app.repository.UserRepository;
+
+import java.util.List;
+
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+    public void deleteById(int id){ userRepository.deleteById(id);}
+
+    public void update(User user){ userRepository.update(user);}
+
+    public User getUser(int id){ return userRepository.getUser(id);}
+}
